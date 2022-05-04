4805b91608d840da4ac6ca858c23466cfb90bfcd
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-03 21:26:52 -0300

    correccion de codigo mejore codigo HTML y .TS

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 rotulo1.innerHTML = "Por favor ingrese el usuario:";
 rotulo2.innerHTML = "Por favor ingrese la clave:";
 
-let usuario: string = "juan";
-let clave: string = "clavejuan";
-let userIngresado: string = dato1.value;
-let claveIngresada: string = dato2.value;
-
-if (usuario == userIngresado && clave == claveIngresada) {
-  console.log("Bienvenido");
-} else {
-  console.log("El usuario o la clave son incorrectos");
-}
+btnIngresar.addEventListener("click", () => {
+  let usuario: string = "juan";
+  let clave: string = "clavejuan";
+  let userIngresado: string = dato1.value;
+  let claveIngresada: string = dato2.value;
+  if (usuario === userIngresado && clave === claveIngresada) {
+    console.log("Bienvenido");
+  } else {
+    console.log("El usuario o la clave son incorrectos");
+  }
+});
